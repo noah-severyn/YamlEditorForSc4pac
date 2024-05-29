908409feff210bf7b5e341b216f547989bd07f39
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2024-05-28 23:27:22 -0400

    move package fieldsets to tabs

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -117,3 +117,24 @@ function CountItems() {
 
 	document.getElementById('CurrentItemCount').innerHTML = 'This file contains: ' + countOfPackages + ' packages, ' + countOfAssets + ' assets'
 }
+
+
+function OpenTab(event, tabName) {
+	var i, tablinks;
+
+	// Get all elements with class="tabcontent" and hide them
+	var tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tab-link");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = "block";
+	event.currentTarget.className += " active";
+} 
